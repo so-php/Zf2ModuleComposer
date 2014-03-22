4da1571d45a375e5f38769d11291f5250ca2b11d
Jeremy Giberson <jeremyg@webpt.com>
2014-03-22 13:24:16 -0700

    added finderTest stub

---
+++ test/Zf2ModuleComposerTest/Unit/Composer/FinderTest.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Zf2ModuleComposerTest\Unit\Composer;
+
+
+class FinderTest extends \PHPUnit_Framework_TestCase {
+
+    public function setUp() {
+        parent::setUp();
+    }
+    
+}
+ 
